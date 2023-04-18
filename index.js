c6e9b67ce8bc7bdcb6c07964a9a43aa64fd9881a
luo <sam9310505@gmail.com>
2023-04-18 20:21:49 +0800

    refactor: check style with eslint

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 require('dotenv').config();
-const { updateRow } = require('./googleSheet.js');
-const { createLoggingInfo } = require('./googleSheet.js');
-const { getCurrentTime } = require('./date.js');
+const {updateRow} = require('./googleSheet.js');
+const {createLoggingInfo} = require('./googleSheet.js');
+const {getCurrentTime} = require('./date.js');
 
-const { Client, GatewayIntentBits } = require('discord.js');
+const {Client, GatewayIntentBits} = require('discord.js');
 const client = new Client({
   intents: [
     // GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildVoiceStates,
     GatewayIntentBits.GuildPresences,
-  ]
+  ],
 });
 
 client.on('ready', () => {
@@ -24,15 +24,13 @@ client.on('voiceStateUpdate', (oldState, newState) => {
   if (newState.channelId && !oldState.channelId) {
     console.log('Someone joined');
     console.log(newState.member.nickname);
-    
-    //write connect time to user
+
+    // write connect time to user
     updateRow(newState.member.id, newState.member.nickname, getCurrentTime());
-  }
-  else if (oldState.channelId && !newState.channelId) {
+  } else if (oldState.channelId && !newState.channelId) {
     console.log('Someone left');
     console.log(oldState.member.nickname);
-  }
-  else {
+  } else {
     console.log('Neither of the two actions occured');
     // ...
   }
@@ -42,9 +40,10 @@ client.on('presenceUpdate', (oldPresence, newPresence) => {
   if (newPresence && newPresence.status === 'online') {
     console.log(newPresence.member.id);
     console.log(newPresence.clientStatus);
-    let user = client.users.cache.get(newPresence.member.id);
-    console.log(user.username)
-    createLoggingInfo(newPresence.member.id, user.username, getCurrentTime(), JSON.stringify(newPresence.clientStatus));
+    const user = client.users.cache.get(newPresence.member.id);
+    console.log(user.username);
+    createLoggingInfo(newPresence.member.id, user.username, getCurrentTime(),
+        JSON.stringify(newPresence.clientStatus));
   }
 });
 
